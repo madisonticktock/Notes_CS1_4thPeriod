7c0e61c8af19fda9974f2658575531aee8057ef8
Madison Tindoc <mtindoc@newtech.coppellisd.com>
2021-09-02 15:22:36 -0500

    Update Notes01_Printing_and_Escape_Sequences.java

--- src/notes/Notes01_Printing_and_Escape_Sequences.java
+++ src/notes/Notes01_Printing_and_Escape_Sequences.java
@@ -4,7 +4,10 @@ public class Notes01_Printing_and_Escape_Sequences {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.print("Hello.");
+		System.out.println(" How are you today?");
+		System.out.println("Mr.Rummel said, \"DO IT AGIAN!\"");
+		System.out.println("When I was \na young boy");
 		
 	}
 	
